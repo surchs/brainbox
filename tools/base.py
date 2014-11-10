a2bbf7e54067f5d344a650a7044d64b721a81167
surchs <sebastian.urchs@gmail.com>
2014-11-10 16:05:16 -0500

    Counter counts more now

--- tools/base.py
+++ tools/base.py
@@ -17,6 +17,7 @@ class Counter(object):
         self.took = 0
         self.cur_tic = time.time()
         self.cur_toc = time.time()
+        self.all = 0
 
     def tic(self):
         self.cur_tic = time.time()
@@ -27,22 +28,32 @@ class Counter(object):
         self.took = self.cur_toc - self.cur_tic
         self.times = np.append(self.times, self.took)
         self.avg = np.average(self.times)
+        self.all = self.cur_toc - self.start
 
     def progress(self, remaining=None):
         if not remaining and not self.total:
-            sys.stdout.write('\r {0} done, avg {1:.2f} seconds.'.format(self.count,
-                                                                    self.avg))
+            sys.stdout.write('\r {0} done, avg {1:.2f} '
+                             'seconds.'.format(self.count,
+                                               self.avg))
             return
         elif remaining and not self.total:
             rem_time = self.avg * remaining
-            sys.stdout.write('\r {0:.2f} seconds to go.'.format(rem_time))
+            sys.stdout.write('\r {0:.2f} seconds to go. '
+                             'One step takes {1:.5f} '
+                             'and we ran for {2:.2f} so far'.format(rem_time,
+                                                                    self.avg,
+                                                                    self.all))
         else:
             remaining = self.total - self.count
             p_complete = float(self.count) / self.total * 100
             rem_time = self.avg * remaining
 
-        sys.stdout.write('\r {0:.1f} % done {1:.2f} seconds to go.'.format(p_complete,
-                                                                   rem_time))
+        sys.stdout.write('\r {0:.1f} % done {1:.2f} seconds to go. '
+                         'One step takes {2:.5f} '
+                         'and we ran for {3:.2f} so far'.format(p_complete,
+                                                                rem_time,
+                                                                self.avg,
+                                                                self.all))
         sys.stdout.flush()
 
     def total(self):
